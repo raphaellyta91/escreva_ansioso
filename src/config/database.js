e99bc2e716d076406d4cd60beb7f158e55626ae2
raphaellyta91 <raphaellyta91@gmail.com>
2026-05-27 15:30:57 -0300

    Migração para SQLite

--- src/config/database.js
+++ src/config/database.js
@@ -1,26 +1,33 @@
-import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
+// import { Sequelize } from 'sequelize';
+// import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
-const sequelize = new Sequelize(
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-   username:   process.env.DB_USER || 'root',
-    password:   process.env.DB_PASSWORD || 'BemVindo!',
-    database:   process.env.DB_NAME || 'ansioso'
-}
-);
+// const sequelize = new Sequelize(
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: 'mysql',
+//    username:   process.env.DB_USER || 'root',
+//     password:   process.env.DB_PASSWORD || 'BemVindo!',
+//     database:   process.env.DB_NAME || 'ansioso'
+// }
+// );
   
-export async function criarBD(){
-await sequelize.query(  'CREATE DATABASE IF NOT EXISTS ANSIOSO;');
- console.log('Banco de dados criado ou já existe.');}
+// export async function criarBD(){
+// await sequelize.query(  'CREATE DATABASE IF NOT EXISTS ANSIOSO;');
+//  console.log('Banco de dados criado ou já existe.');}
 
 
-criarBD()
+// criarBD()
+
+// export default sequelize;
+
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: './database.sqlite'
+});
 
 export default sequelize;
-
-
 
