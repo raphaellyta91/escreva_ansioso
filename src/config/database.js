af45933cd6defd3be58f72712417d943897ee55b
raphaellyta91 <raphaellyta91@gmail.com>
2026-06-02 18:35:06 -0300

    login_feito

--- src/config/database.js
+++ src/config/database.js
@@ -1,33 +1,24 @@
-// import { Sequelize } from 'sequelize';
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
-// const sequelize = new Sequelize(
-//   {
-//     host: process.env.DB_HOST,
-//     dialect: 'mysql',
-//    username:   process.env.DB_USER || 'root',
-//     password:   process.env.DB_PASSWORD || 'BemVindo!',
-//     database:   process.env.DB_NAME || 'ansioso'
-// }
-// );
-  
-// export async function criarBD(){
-// await sequelize.query(  'CREATE DATABASE IF NOT EXISTS ANSIOSO;');
-//  console.log('Banco de dados criado ou já existe.');}
-
-
-// criarBD()
-
-// export default sequelize;
-
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite'
-});
+dotenv.config();
+
+const sequelize = new Sequelize(
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+   username:   process.env.DB_USER || 'root',
+    password:   process.env.DB_PASSWORD || 'BemVindo!',
+    database:   process.env.DB_NAME || 'ansioso'
+}
+);
+  
+export async function criarBD(){
+await sequelize.query(  'CREATE DATABASE IF NOT EXISTS ANSIOSO;');
+ console.log('Banco de dados criado ou já existe.');}
+
+
+criarBD()
 
 export default sequelize;
 
