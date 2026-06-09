38052a2f2cde484bc45bfb1f3def8efa1d41abce
raphaellyta91 <raphaellyta91@gmail.com>
2026-06-09 20:29:21 -0300

    Configura SQLite para deploy no Render

--- src/config/database.js
+++ src/config/database.js
@@ -1,24 +1,8 @@
 import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-const sequelize = new Sequelize(
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-   username:   process.env.DB_USER || 'root',
-    password:   process.env.DB_PASSWORD || 'BemVindo!',
-    database:   process.env.DB_NAME || 'ansioso'
-}
-);
-  
-export async function criarBD(){
-await sequelize.query(  'CREATE DATABASE IF NOT EXISTS ANSIOSO;');
- console.log('Banco de dados criado ou já existe.');}
-
-
-criarBD()
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: './src/database/database.sqlite'
+});
 
 export default sequelize;
-
