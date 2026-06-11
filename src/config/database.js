263552a738e193bc28c773aaba40f1f85e3ba77f
raphaellyta91 <raphaellyta91@gmail.com>
2026-06-11 20:07:43 -0300

    Migra para PostgreSQL

--- src/config/database.js
+++ src/config/database.js
@@ -3,18 +3,33 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DATABASE_URL,
-  {
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    },
-    logging: false
-  }
-);
+let sequelize;
+if(process.env.MODE_NODE === 'dev'){
+
+    console.log('Modo: ', process.env.MODE_NODE)
+
+    sequelize = new Sequelize({
+        dialect: 'sqlite',
+        storage: './src/database/bd.sqlite'
+    })
+
+}else{
+
+    console.log('Modo: ', process.env.MODE_NODE)
+
+
+
+
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  logging: false
+});
 
 export default sequelize;
